86309e8af28de5f71a2e70830b9ce396eda35ce6
Shuntian (Eddie) Li <shuntian.li@acera.ca>
2025-08-05 17:12:14 -0600

    add path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 # Load environment variables from .env file
 
-# app_password = os.getenv("Google_App_Password")
+# app_password = os.getenv("Google_App_Password")git
 # receiver_email = os.getenv("receiver_email")
 # sender_email = os.getenv("sender_email")
 
@@ -18,7 +18,6 @@ app_password = st.secrets["Google_App_Password"]
 # print(f"Receiver Email: {receiver_email}")  
 # print("✅ App Password loaded successfully!")
 
-#%%
 
 # Set page configuration
 st.set_page_config(page_title="FA - Flower", layout="wide")
@@ -49,18 +48,18 @@ if page == "FA - Flower":
 
     # Sample flower shop items (Name, Price, Image URL)
     flowers = [
-        {"name": "Blush Elegance", "price": 65, "img": ".\\img_trim\\1.jpg"},
-        {"name": "Sunlit Grace", "price": 74, "img": ".\\img_trim\\2.jpg"},
-        {"name": "Enchanted Orchids", "price": 100, "img": ".\\img_trim\\3.jpg"},
-        {"name": "Golden Sunrise", "price": 68, "img": ".\\img_trim\\4.jpg"},
-        {"name": "Blush Harmony", "price": 76, "img": ".\\img_trim\\5.jpg"},
-        {"name": "Spring Serenade", "price": 83, "img": ".\\img_trim\\6.jpg"},
-        {"name": "Whispering Love", "price": 64, "img": ".\\img_trim\\7.jpg"},
-        {"name": "Midnight Elegance", "price": 85, "img": ".\\img_trim\\8.jpg"},
-        {"name": "Frosted Grace", "price": 88, "img": ".\\img_trim\\9.jpg"},
-        {"name": "Whispering Pastels", "price": 78, "img": ".\\img_trim\\10.jpg"},
-        {"name": "Grace in Bloom", "price": 75.5, "img": ".\\img_trim\\11.jpg"},
-        {"name": "Champagne Whispers", "price": 66.5, "img": ".\\img_trim\\12.jpg"},
+        {"name": "Blush Elegance", "price": 65, "img": "./img_trim1.jpg"},
+        {"name": "Sunlit Grace", "price": 74, "img": "./img_trim2.jpg"},
+        {"name": "Enchanted Orchids", "price": 100, "img": "./img_trim3.jpg"},
+        {"name": "Golden Sunrise", "price": 68, "img": "./img_trim4.jpg"},
+        {"name": "Blush Harmony", "price": 76, "img": "./img_trim5.jpg"},
+        {"name": "Spring Serenade", "price": 83, "img": "./img_trim6.jpg"},
+        {"name": "Whispering Love", "price": 64, "img": "./img_trim7.jpg"},
+        {"name": "Midnight Elegance", "price": 85, "img": "./img_trim8.jpg"},
+        {"name": "Frosted Grace", "price": 88, "img": "./img_trim9.jpg"},
+        {"name": "Whispering Pastels", "price": 78, "img": "./img_trim10.jpg"},
+        {"name": "Grace in Bloom", "price": 75.5, "img": "./img_trim11.jpg"},
+        {"name": "Champagne Whispers", "price": 66.5, "img": "./img_trim12.jpg"},
     ]
 
     cols_per_row = 4
